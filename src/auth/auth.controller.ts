d9d4284bfc6bc5ac1c5261a8ea64ce5dca42c242
Gaurav Ghanwat <gauravghanwat2001@gmail.com>
2023-05-11 14:26:47 +0530

    Authenticate user using JWT Service

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    
+    @Post('login')
+    signIn(@Body() signInDto: Record<string, any>) {
+      return this.authService.signIn(signInDto.email, signInDto.password);
+    }
+}
